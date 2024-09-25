38d728d70a4a59c1b5013d0e30dd7fe95937f25d
Asmallg <asmaibboupro@gmail.com>
2024-09-25 11:07:19 +0200

    database v3

--- db.js
+++ db.js
@@ -1,21 +1,32 @@
 const { Sequelize } = require('sequelize')
 
+const express = require('express');
 const { Pool } = require('pg');
 require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false 
   }
 });
 
-pool.connect((err) => {
-  if (err) {
-    console.error('Erreur de connexion à la base de données :', err.stack);
-  } else {
-    console.log('Connecté à la base de données PostgreSQL');
+app.get('/test-db', async (req, res) => {
+  try {
+
+    const result = await pool.query('SELECT NOW()');
+    res.status(200).json({ status: 'Success', time: result.rows[0] });
+  } catch (err) {
+    console.error('Erreur de connexion à la base de données:', err);
+    res.status(500).json({ status: 'Error', message: 'Erreur de connexion à la base de données' });
   }
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur le port ${port}`);
 });
 
 // database
